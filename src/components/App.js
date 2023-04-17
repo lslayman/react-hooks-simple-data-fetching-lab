467ce251abc5a122b15fc2ce893507cec86d1194
Lauren Slayman <laurenaslayman@gmail.com>
2023-04-17 16:39:55 -0600

    passes tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [randomDog, setRandomDog] = useState(null);
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) => {
+            setRandomDog(data.message);
+        });
+    }, []);
+
+    if (!randomDog) return <p>Loading...</p>
+
+    return <img src={randomDog} alt="A Random Dog" />
+}
+
+export default App;
